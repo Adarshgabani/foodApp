abae58a2549010ceba526837ec5de7d7a0c45678
Adarsh Gabani <56997402+Adarshgabani@users.noreply.github.com>
2020-10-16 18:25:16 +0530

    update Search Api Key
    when ever try to access this file make sure to replace SearchAPIKey with api key sring with bearer word

--- src/api/yelp.js
+++ src/api/yelp.js
@@ -1,1 +1,11 @@
 
+import axios from 'axios';
+
+
+
+export default axios.create({
+    baseURL: 'https://api.yelp.com/v3/businesses',
+    headers: {
+        Authorization: SearchAPIKey
+    }
+}); 
